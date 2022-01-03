ec90ea1688bb41864ff43505d17af3d59a634412
MortenHansen <nichlash3@gmail.com>
2022-01-03 20:22:24 +0100

    Added Cloud config file for the Digital Ocean server

--- HairDresserAPI/Controllers/DigitalOceanController.cs
+++ HairDresserAPI/Controllers/DigitalOceanController.cs
@@ -5,16 +5,27 @@ using Microsoft.AspNetCore.Mvc;
 namespace HairDresserAPI.Controllers;
 
 [ApiController]
+[Route("/DigitalOcean/")]
 public class DigitalOceanController
 {
-    public string DigitalOceanToken { get; set; }
+    private string DigitalOceanToken { get; set; }
+    private string UserData { get; set; }
+    private string PathToYamlFile { get; set; }
 
+    
     public DigitalOceanController()
     {
         DigitalOceanToken = new ApiCredentials().DigitalOceanAPIToken;
+        PathToYamlFile = $"{AppDomain.CurrentDomain.BaseDirectory}cloud-config.yaml";
+        UserData = new StreamReader(PathToYamlFile).ReadToEnd();
     }
-    public async Task<HttpStatusCode> CreateDroplet(string name,string region, string size, string image,string tag)
+    [HttpPost]
+    [Route("CreateDroplet")]
+    public async Task<HttpStatusCode> CreateDroplet(string name,string tag)
     {
+        string region = "fra1";
+        string size = "s-1vcpu-1gb";
+        string image = "ubuntu-21-04-x64";
         
         var digitalOcean = new DigitalOcean.API.DigitalOceanClient(DigitalOceanToken);
         var request = new Droplet {
@@ -22,7 +33,8 @@ public class DigitalOceanController
             Region = region,
             Size = size,
             Image = image,
-            Tags = new List<string> { tag }
+            Tags = new List<string> { tag },
+            UserData = UserData
         };
         var droplet = await digitalOcean.Droplets.Create(request);
 
--- HairDresserTests/DigitalOceanTest.cs
+++ HairDresserTests/DigitalOceanTest.cs
@@ -12,13 +12,10 @@ public class DigitalOceanTest
         //Arrange
         var digitalOceanController = new DigitalOceanController();
         string name = "Test-Droplet";
-        string region = "fra1";
-        string sizeSlug = "s-1vcpu-1gb";
-        string image = "ubuntu-21-04-x64";
         string tag = "Test DROPLET";
         
         //Act
-        var responseCode = digitalOceanController.CreateDroplet(name,region,sizeSlug,image,tag);
+        var responseCode = digitalOceanController.CreateDroplet(name,tag);
         
         //Assert
         Assert.Equal(HttpStatusCode.Accepted,responseCode.Result);
