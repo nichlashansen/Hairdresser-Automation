73182ca841458e486ba6eb49250d19ec1287ecce
MortenHansen <nichlash3@gmail.com>
2022-01-04 13:26:12 +0100

    Dns update test working

--- HairDresserAPI/Controllers/CloudFlareController.cs
+++ HairDresserAPI/Controllers/CloudFlareController.cs
@@ -1,35 +1,116 @@
+using System.Text.Json;
 using CloudFlare.Client;
 using CloudFlare.Client.Api.Authentication;
 using CloudFlare.Client.Api.Result;
 using CloudFlare.Client.Api.Zones;
+using CloudFlare.Client.Api.Zones.DnsRecord;
+using CloudFlare.Client.Client.Zones;
+using CloudFlare.Client.Contexts;
+using CloudFlare.Client.Enumerators;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HairDresserAPI.Controllers;
-[ApiController]
 [Route("/Cloudflare")]
+[ApiController]
 public class CloudFlareController
 {
     private CloudFlareClient CloudFlare { get; set; }
-    private ApiKeyAuthentication CloudflareAuth{ get; set; }
-    private string cloudflareEmail { get; set; }
-    private string cloudflareApi { get; set; }
+    private string CloudflareEmail { get; set; }
+    private string CloudflareApi { get; set; }
     public CloudFlareController()
     {
-        cloudflareEmail = new ApiCredentials().CloudflareEmail;
-        cloudflareApi = new ApiCredentials().CloudflareToken;
-        CloudflareAuth = new ApiKeyAuthentication(cloudflareEmail, cloudflareApi);
-        CloudFlare = new CloudFlareClient(CloudflareAuth);
+        CloudflareEmail = new ApiCredentials().CloudflareEmail;
+        CloudflareApi = new ApiCredentials().CloudflareToken;
+        CloudFlare = new CloudFlareClient(CloudflareEmail, CloudflareApi);
 
     }
 
     [HttpPost]
     [Route("AddDomain")]
-    public async Task<bool> AddDomain(string domainName)
+    public async Task<CloudFlareResult<Zone>> AddDomain(string domainName)
     {
-        NewZone zone = new NewZone();
-        zone.Name = domainName;
-        zone.JumpStart = true;
-        var result = await CloudFlare.Zones.AddAsync(zone);
-        return result.Success;
+        using (CloudFlare)
+        {
+            NewZone zone = new NewZone();
+            zone.Name = domainName;
+            zone.JumpStart = true;
+            var result = await CloudFlare.Zones.AddAsync(zone);
+            return result;
+        }
+        
     }
+
+    [HttpPut]
+    [Route("UpdateDNS")]
+    public async Task<IResult> UpdateDomainDNS(string domainName, string serverIp)
+    {
+        using (CloudFlare)
+        {
+            var zoneIdTask = GetZoneId(domainName);
+            
+            NewDnsRecord wwwDnsRecord = new NewDnsRecord()
+            {
+                Type = DnsRecordType.A,
+                Content = serverIp,
+                Name = "www." + domainName,
+                Ttl = 1,
+                Proxied = true
+            };
+
+            NewDnsRecord newDnsRecord = new NewDnsRecord()
+            {
+                Type = DnsRecordType.A,
+                Content = serverIp,
+                Name = domainName,
+                Ttl = 1,
+                Proxied = true
+            };
+
+            var _ = await CheckIfARecordsExistsAndDeletes(zoneIdTask.Result);
+            var baseDns = await CloudFlare.Zones.DnsRecords.AddAsync(zoneIdTask.Result,newDnsRecord);
+            var addAsync = await CloudFlare.Zones.DnsRecords.AddAsync(zoneIdTask.Result,wwwDnsRecord);
+            
+            if (addAsync.Success && baseDns.Success)
+            {
+                return Results.Ok();
+            }
+            
+            return Results.Problem(addAsync.Errors.ToString());
+
+
+        }
+    }
+
+
+  private async Task<string> GetZoneId(string domainName)
+{
+        ZoneFilter filter = new ZoneFilter();
+        filter.Name = domainName;
+        var result = await CloudFlare.Zones.GetAsync(filter);
+        var correctZone = result.Result[0];
+        return correctZone.Id;
 }
+
+  private async Task<bool> CheckIfARecordsExistsAndDeletes(string zoneId)
+  {
+      var filter = new DnsRecordFilter();
+      filter.Type = DnsRecordType.A;
+      var result = await CloudFlare.Zones.DnsRecords.GetAsync(zoneId, filter);
+      if (result.Result.Count !=0)
+      {
+          DeleteExistingARecords(result);
+      }
+
+      return true;
+  }
+
+  private async void DeleteExistingARecords(CloudFlareResult<IReadOnlyList<DnsRecord>> dnsRecords)
+  {
+      foreach (var dns in dnsRecords.Result)
+      {
+          await CloudFlare.Zones.DnsRecords.DeleteAsync(dns.ZoneId, dns.Id);
+      }
+  }
+
+}
+
--- HairDresserTests/CloudFlareTest.cs
+++ HairDresserTests/CloudFlareTest.cs
@@ -1,24 +1,44 @@
 using System.Net;
+using CloudFlare.Client.Api.Zones;
 using HairDresserAPI.Controllers;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
 using Xunit;
 
 namespace HairDresserTests;
 
 public class CloudFlareTest
 {
+    //use same domain for adding, and updating
+    private string DomainName = "mogensjensenertest.dk";
+    
     [Fact]
     public void AddCloudFlareDomainTest()
     {
         //Arrange
-        string domainName = "mogensjensenertest.dk";
-        var Cloudflare = new CloudFlareController();
+        var cloudflare = new CloudFlareController();
 
         //Act
-        var json = Cloudflare.AddDomain(domainName);
-        
+        var json = cloudflare.AddDomain(DomainName);
+        var result = json.Result;
 
         //Assert
-        bool result = json.Result;
-        Assert.True(result);
+        Assert.Equal(DomainName,result.Result.Name);
+    }
+
+    [Fact]
+    public async void UpdateDomainTest()
+    {
+        //Arrange
+        string serverIp = "104.131.95.246";
+        var cloudflare = new CloudFlareController();
+        
+        //Act
+        var json = await cloudflare.UpdateDomainDNS("betterback.dk",serverIp);
+
+        //Assert
+        Assert.NotStrictEqual(Results.Ok(),json);
+
+
     }
 }
